494f8d27550665e3b7e7ef286e1463372b6604bd
synasharma <61623971+synasharma@users.noreply.github.com>
2021-04-15 14:45:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function next()
+{
+    window.location="index2.html";
+}
+function next2()
+{
+    window.location="index3.html";
+}
+function next3()
+{
+    window.location="index4.html";
+}
+function next4()
+{
+    window.location="index5.html";
+}
